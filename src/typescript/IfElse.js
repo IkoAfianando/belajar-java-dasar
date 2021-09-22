87ed6c70713be0db221e328227296a603dbb4086
IkoAfianando <ikoafianando123@gmail.com>
2021-09-22 08:09:28 +0700

    Pembahruan penyempurnaan

--- src/typescript/IfElse.js
+++ src/typescript/IfElse.js
@@ -1,10 +1,13 @@
-function fizzbuzz(data){
-    for(let i = 1; i <= data; i++){
-        if(i % 3 === 0 && i % 5 === 0){
+function fizzbuzz(jumlah){
+    for(let i = 1; i <= jumlah; i++){
+        const data1 = i % 3 === 0 && i % 5 === 0
+        const data2 = i % 3 === 0;
+        const data3 = i % 5 === 0;
+        if(data1){
             console.log(`FizzBuzz ${i}`);
-        }else if(i % 3 === 0){
+        }else if(data2){
             console.log(`Fizz ${i}`);
-        }else if(i % 5 === 0){
+        }else if(data3){
             console.log(`Buzz ${i}`);
         }else {
             console.log(i);
@@ -12,4 +15,4 @@ function fizzbuzz(data){
     }
 }
 
-fizzbuzz(200);
+fizzbuzz(100);
